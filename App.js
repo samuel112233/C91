cbf6e89ef090b001f085fd431b3442a7017f16d6
samuel112233 <samuelbhowmik@gmail.com>
2021-02-14 12:56:36 +0530

    screens

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {createAppContainer,createSwitchNavigator} from 'react-navigation';
+import SignUpScreen from './screens/SignUpScreen';
+import LoginScreen from './screens/LoginScreen';
+ 
+export default class App extends React.Component{
+  render(){
+    return(
+     <AppContainer/>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+var AppNavigator = createSwitchNavigator({
+  SignUp:{screen:SignUpScreen},
+  Login:{screen:LoginScreen}
+})
+var AppContainer = createAppContainer(AppNavigator)
